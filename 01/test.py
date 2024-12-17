1c9a384374f868524afc5da490670252bd1afec8
itljan30 <itljan3012002@gmail.com>
2024-12-17 04:06:25 -0500

    Fixed global CMakeLists.txt to use new file names

--- 01/test.py
+++
@@ -1,9 +0,0 @@
-list1 = []
-list2 = []
-
-with open("input.txt", "r") as file:
-    for line in file:
-        numbers = line.split("   ")
-
-        list1.append(numbers[0])
-        list2.append(numbers[1])
